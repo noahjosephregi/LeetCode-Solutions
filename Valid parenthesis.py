4cdc65fc619773ff1b9cea41f03970eace311436
noahjosephregi <noahregip@gmail.com>
2026-06-06 12:28:30 +0530

    solved: Valid parenthesis

---
+++ Valid parenthesis.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self,s):
+        i=0
+        a=[]
+        for i in range(len(s)):
+            if s[i]=='(' or s[i]=='{' or s[i]=='[':
+                a.append(s[i])
+            else:
+                if not a:
+                    return False
+                top=a.pop()
+                if s[i]==')' and top!='(':
+                    return False
+                if s[i]=='}' and top!='{':
+                    return False
+                if s[i]==']' and top!='[':
+                    return False   
+        return len(a)==0        
+        
+        
